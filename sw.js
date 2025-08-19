d08ca8718f89eeeafff2d01624cafcdfee97d044
NAR <yyazok@gmail.com>
2025-08-19 22:04:11 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
 self.addEventListener("fetch", (event) => {
   const url = new URL(event.request.url);
 
-  // Sadece "devnar.github.io/masa" altındaki dosyaları yakala
   if (url.pathname.startsWith("/")) {
       event.respondWith(
           caches.match(event.request).then((response) => {
